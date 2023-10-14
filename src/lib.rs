2c0e379b67f9ae8c780255045f3459cefd4c5618
Tomás Lucas <tomaslucas@gmail.com>
2023-10-14 14:35:35 +0200

    paso 3 completo

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use std::{error::Error, fs};
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+impl Config {
+    pub fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments.");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Self { query, file_path })
+    }
+}
