8800e77e604b22f3cf9bf07987651df15b5c8cf4
Tomás Lucas <tomaslucas@gmail.com>
2023-10-14 15:03:21 +0200

    paso 4 completo

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,5 @@
 use std::{error::Error, fs};
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-    Ok(())
-}
-
 pub struct Config {
     pub query: String,
     pub file_path: String,
@@ -19,5 +12,40 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
         Ok(Self { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+    Ok(())
+}
+
+fn search<'search>(query: &str, contents: &'search str) -> Vec<&'search str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
